c4b11d3d9f446e7d26d43529babcc04c6754bd96
xzqiaochu <xzzhangqiaochu@163.com>
2019-07-08 17:48:29 +0800

    Update example exgcd.cpp

--- example/gcd/exgcd.cpp
+++ example/gcd/exgcd.cpp
@@ -1,7 +1,11 @@
-//扩展欧几里得算法
+/*  扩展欧几里得算法 = 求逆元
+时间复杂度: O(log n)
+最后修改时间: 2019-07-08 */
 #include <cstdio>
 
-int exgcd(int a, int b, int &x, int &y) //这里 int &x, int &y 表示引用 
+// 求解 a*x + b*y = gcd(a, b)
+// 返回值为 a, b 的最大公约数
+int exgcd(int a, int b, int &x, int &y) //这里 int &x, int &y 表示引用
 {
     if (b == 0)
     {
@@ -11,6 +15,14 @@ int exgcd(int a, int b, int &x, int &y) //这里 int &x, int &y 表示引用
     int gcd = exgcd(b, a % b, y, x);
     y -= a / b * x;
     return gcd;
+}
+
+// 求 a (mod b) 的逆元
+int inv(int a, int b)
+{
+    int x, y;
+    exgcd(a, b, x, y);
+    return x;
 }
 
 int main()
