d1226e06e6435273d00686678ab88c82c18f1335
xzqiaochu <xzzhangqiaochu@163.com>
2019-10-30 22:27:29 +0800

    更新 评测程序&对拍程序

---
+++ judge/ptd/judge.cpp
@@ -0,0 +1,70 @@
+// 评测主程序（program to data)
+// 注意事项：
+// 1. 程序直接用标准输入输出即可
+// 2. (name).cpp 每次运行时会自动编译
+// 3. 比较时会过滤掉行末空格和尾行回车
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+const int SZ = 107;
+
+char name[SZ], tmp[SZ];
+
+int main() {
+    puts("The name of program");
+    scanf("%s", name);
+
+	// 编译程序
+    sprintf(tmp, "Compiling %s.cpp...", name);
+	puts(tmp);
+	// 我这里是最严格的编译，所有警告都会被视作致命错误
+    sprintf(tmp, "g++ %s.cpp -o %s.exe -Wall -Wextra -Werror", name, name);
+	// sprintf(tmp, "g++ %s.cpp -o %s.exe -Wall", name, name);
+	if (system(tmp))
+		return 0;
+	puts("Compiled");
+
+	// 开始评测
+	for (int i = 0; ; i++) {
+		// 判断测试带点是否存在
+		sprintf(tmp, "data\\%s%d.in", name, i);
+		if (FILE *file = fopen(tmp, "r"))
+			fclose(file);
+		else
+			break;
+		sprintf(tmp, "data\\%s%d.out", name, i);
+		if (FILE *file = fopen(tmp, "r"))
+			fclose(file);
+		else
+			break;
+
+		// 复制测试点
+        sprintf(tmp, "copy data\\%s%d.in %s.in >nul", name, i, name);
+        system(tmp);
+        sprintf(tmp, "copy data\\%s%d.out %s.ans >nul", name, i, name);
+        system(tmp);
+
+		// 运行程序
+        sprintf(tmp, "%s.exe < %s.in > %s.out", name, name, name);
+		double st = clock();
+		system(tmp);
+		double ed = clock();
+
+		// 比对结果
+        sprintf(tmp, "fc %s.out %s.ans /w >nul", name, name);
+		if (system(tmp)) {
+			printf("Wrong Answer, Case #%d, Time %.0fms\n", i, ed - st);
+			return 0;
+		} else {
+			printf("Accepted, Case #%d, Time %.0fms\n", i, ed - st);
+		}
+	}
+	// 删除测试点文件（刚才复制的）
+	sprintf(tmp, "del %s.in >nul", name);
+	system(tmp);
+	sprintf(tmp, "del %s.out >nul", name);
+	system(tmp);
+	sprintf(tmp, "del %s.ans >nul", name);
+	system(tmp);
+}
---
+++ judge/ptp/judge.cpp
@@ -0,0 +1,48 @@
+// 对拍主程序（program to program)
+// 注意事项：
+// 1. 程序直接用标准输入输出即可
+// 2. my.cpp 每次运行时会自动编译
+// 3. radmon.cpp 和 std.cpp 请手动编译
+// 2. 比较时会过滤掉行末空格和尾行回车
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+int n;
+
+int main() {
+	puts("The number of cases");
+	scanf("%d", &n);
+
+	// 编译程序
+	puts("Compiling my.cpp...");
+	// 我这里是最严格的编译，所有警告都会被视作致命错误
+	if (system("g++ my.cpp -o my.exe -Wall -Wextra -Werror"))
+	// if (system("g++ my.cpp -o my.exe -Wall"))
+		return 0;
+	puts("Compiled");
+
+	// 开始对拍
+	for (int i = 1; i <= n; i++) {
+		// 产生数据
+		system("random.exe > data.in");
+		
+		// 运行程序
+		double st = clock();
+		system("my.exe < data.in > data.out");
+		double ed = clock();
+		system("std.exe < data.in > data.ans");
+		
+		// 比对
+		if (system("fc data.out data.ans /w >nul")) {
+			printf("Wrong Answer, Case #%d, Time %.0fms\n", i, ed - st);
+			return 0;
+		} else {
+			printf("Accepted, Case #%d, Time %.0fms\n", i, ed - st);
+		}
+	}
+	// 删除测试点文件
+	system("del data.in >nul");
+	system("del data.out >nul");
+	system("del data.ans >nul");
+} 
