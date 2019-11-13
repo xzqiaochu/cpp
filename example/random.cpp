2738ea453ad0e39b00122c4d7802f2c80792cabf
xzqiaochu <xzzhangqiaochu@163.com>
2019-11-13 20:17:54 +0800

    更新example

--- example/random.cpp
+++ example/random.cpp
@@ -1,17 +1,21 @@
-﻿//毫秒级随机数测试 
-#include <iostream>
+﻿/* 随机数生成器
+描述：本程序采用毫米级随机数种子，保证两次运行所产生的随机数序列不同
+用途：测试点生成
+注意事项：比赛/做题 时请不要这样写，原因如下：
+1. ftime函数不一定能通过编译
+2. 有些考察随机数的题目，直接用rand()就行，题目的数据是设计好的（用srand()反而通过不了) */
+#include <cstdio>
 #include <cstdlib>
 #include <ctime>
-
-using namespace std;
 
 int rdm(int a, int b) {
 	return (rand() % (b - a + 1)) + a;
 }
 
 int main() {
-	struct timeb timeseed;
-	ftime(&timeseed);
+	timeb timeseed; //这是一个结构体
+	ftime(&timeseed); //获取目前时间
+	//设置随机数种子为系统时间，不然的话，每次运行程序获取的随机数序列都是相同的
 	srand(timeseed.time * 1000 + timeseed.millitm);
 	int cnt1 = 0, cnt2 = 0;
 	for (int i = 1; i <= 100000; i++) {
@@ -19,6 +23,6 @@ int main() {
 		if (temp == 0) cnt1++;
 		else cnt2++;
 	}
-	cout << cnt1 << endl << cnt2 << endl;
+	printf("%d %d", cnt1, cnt2);
 	return 0;
 }
--- example/tree/LCA.cpp
+++ example/tree/LCA.cpp
@@ -1,69 +1,85 @@
 /* 最近公共祖先(LCA) - 树上倍增法
 时间复杂度: O(log n)
 出处：算法竞赛-图论-树的直径与最近公共祖先 0x63 P371
-最后修改时间：2019-07-06 */
-
+最后修改时间：2019-11-13 */
 #include <cstdio>
 #include <cmath>
+#include <cstdio>
 #include <queue>
-
-#define next Next
-
-using namespace std;
-
-const int MAXN = 50007, MAXM = 2 * MAXN, MAXK = 25;
+using std::queue;
+const int VSZ = 1e5 + 7, ESZ = 2 * VSZ, MAXT = 25;
 
 int n, m, a, b;
-int tot, head[MAXN], ver[MAXM], edge[MAXM], next[MAXM];
-int t, dis[MAXN], dep[MAXN], f[MAXN][MAXK];
-queue<int> q;
 
-void init()
+int head[VSZ];
+struct Edge {
+    int to, weight, next;
+} edge[ESZ];
+
+bool vis[VSZ];
+int t, dis[VSZ], dep[VSZ], fa[VSZ][MAXT];
+
+void mainInit()
 {
     t = (int)(log2(n)) + 1;
 }
 
-void add(int x, int y, int z)
+void addEdge(int x, int y, int z)
 {
-    ver[++tot] = y, edge[tot] = z;
-    next[tot] = head[x], head[x] = tot;
+    static int tot = 0;
+    edge[++tot].to = y, edge[tot].weight = z;
+    edge[tot].next = head[x], head[x] = tot;
 }
 
+// 清空 vis fa
+void bfsInit() {
+    for (int i = 1; i <= n; i++) {
+        vis[i] = false;
+        for (int k = 1; k <= t; k++)
+            fa[i][k] = 0;
+    }
+}
+
+// 预处理：dep dis fa
 void bfs()
 {
-    q.push(1), dep[1] = 1;
+    bfsInit();
+    queue<int> q;
+    dep[1] = 1, dis[1] = 0, fa[1][0] = 0;
+    vis[1] = true;
+    q.push(1);
     while (!q.empty())
     {
-        int x = q.front();
-        q.pop();
-        for (int i = head[x]; i; i = next[i])
+        int x = q.front(); q.pop();
+        for (int i = head[x]; i; i = edge[i].next)
         {
-            int y = ver[i], z = edge[i];
-            if (dep[y]) // 若边y已被访问，即dep[y] != 0（dep[y] 初始值为0）
+            int y = edge[i].to, z = edge[i].weight;
+            if (vis[y])
                 continue;
+            vis[y] = true;
             dep[y] = dep[x] + 1;
             dis[y] = dis[x] + z;
-            f[y][0] = x;
-            for (int j = 1; j <= t; j++)
-                f[y][j] = f[f[y][j - 1]][j - 1]; // y的2^k倍祖先 = y的2^(k-1)倍祖先 的 2^(k-1)倍祖先
+            fa[y][0] = x;
+            for (int k = 1; k <= t; k++)
+                fa[y][k] = fa[ fa[y][k-1] ][k-1]; // y的2^k倍祖先 = y的2^(k-1)倍祖先 的 2^(k-1)倍祖先
             q.push(y);
         }
     }
 }
 
-int lca(int x, int y)
+int Lca(int x, int y)
 {
-    if (dep[y] > dep[x])
-        swap(x, y);
+    if (dep[x] < dep[y]) // 使得x的深度最大
+        std::swap(x, y);
     for (int i = t; i >= 0; i--)
-        if (dep[f[y][i]] >= dep[x])
-            y = f[y][i];
-    if (x == y)
+        if (dep[fa[x][i]] >= dep[y]) // 调整x（上升），使得x和y的深度相同
+            x = fa[x][i];
+    if (x == y) // 已经是同一个点，直接返回（一定要特判）
         return x;
-    for (int i = t; i >= 0; i--)
-        if (f[x][i] != f[y][i])
-            x = f[x][i], y = f[y][i];
-    return f[x][0];
+    for (int i = t; i >= 0; i--) // x和y一起往上跳
+        if (fa[x][i] != fa[y][i])
+            x = fa[x][i], y = fa[y][i];
+    return fa[x][0]; // 此时x和y的父节点相同，其父节点即为最近公共祖先
 }
 
 int main()
@@ -73,11 +89,12 @@ int main()
     {
         int x, y, z;
         scanf("%d%d%d", &x, &y, &z);
-        add(x, y, z), add(y, x, z);
+        addEdge(x, y, z), addEdge(y, x, z);
     }
     scanf("%d%d", &a, &b);
-    init();
+    mainInit();
     bfs();
-    printf("%d", dis[a] + dis[b] - 2 * dis[lca(a, b)]);
+    int lca = Lca(a, b);
+    printf("%d", dis[a] + dis[b] - 2 * dis[lca]);
     return 0;
 }
